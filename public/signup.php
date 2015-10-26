f716c08f5b288883e1b33fee1e2063a6c828e2f0
RogerLau <liu@MacBook-Pro.local>
2015-10-25 23:57:49 -0700

    Signup page responsive design
    The design of the signup page still need some discussion.

--- public/signup.php
+++ public/signup.php
@@ -19,55 +19,70 @@ if(isset($_SESSION['userauth'])) {
 
 	<link rel = "stylesheet" type = "text/css" href = "css/signup.css">
 
-	<link href='https://fonts.googleapis.com/css?family=Cabin:500' rel='stylesheet' type='text/css'>
- 
-    <link href='https://fonts.googleapis.com/css?family=Itim' rel='stylesheet' type='text/css'>
-    
     <script src = "js/signup_script.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-	<div id = "header">
-	<h1 align="center">Create your very own OurMail Account! It's fast and free!</h1>
+	<div class = "container-fluid" >
+		<div id = "header">
+			<h1 align="center">Create your very own OurMail Account! It's fast and free!</h1>
+		</div>
+		<div class = "row">
+				<div class="Logo" >
+					<h3 id = "sidetext">The power of email all in a single account.</h3>
+					 <div class="col-xs-12 col-md-6">
+					    <a href="" class="thumbnail">
+					      <img src="img/signuplogos/gmail.png" alt="...">
+					    </a>
+					 </div>
+					 <div class="col-xs-12 col-md-6">
+					    <a href="" class="thumbnail">
+					      <img src="img/signuplogos/gmail.png" alt="...">
+					    </a>
+					 </div>
+					 <div class="col-xs-12 col-md-6">
+					    <a href="" class="thumbnail">
+					      <img src="img/signuplogos/gmail.png" alt="...">
+					    </a>
+					 </div>
+					 <div class="col-xs-12 col-md-6">
+					    <a href="" class="thumbnail">
+					      <img src="img/signuplogos/gmail.png" alt="...">
+					    </a>
+					 </div>
+				</div>			
+		</div>
+		<div class="Signup">
+				<form name = "signupform" onsubmit = "return(validation());" >
+					Name:<br>
+					<input class = "name" type = "text" name = "firstname" placeholder = "First" >
+					<input class = "name"type = "text" name = "lastname" placeholder = "Last" ><br><br>
+			    
+			    	Email Address:<br>
+					<input class = "samebox" type = "text" name = "email" placeholder = "Email" autocomplete="on"><br><br>
+				
+					Username:<br>
+					<input class = "samebox" type = "text" name = "username" placeholder = "Username" autocomplete="off"><br><br>
+
+					Password:<br>
+					<input class = "samebox" type = "password" name = "password" placeholder = "Password" autocomplete="off" ><br><br>
+
+					Security Question (case sensitive):<br>
+			    	<select id = "dropbox" name = "security_questions">
+			    	<option value = "number_1 "> What was your mother's maiden name?</option>
+			    	<option value="saab">What was your childhood nickname?</option>
+			    	<option value="fiat">What school did you attend in 6th grade?</option>
+			    	<option value="audi">What was the model of your first car?</option>
+			    	</select>
+			    	<br><br>
+			    	<input class = "samebox" type = "text" name = "security_answer" placeholder = "Answer" autocomplete="off" ><br><br>
+					<input id = "submit" type="submit">
+				</form>
+		</div>
 	</div>
-
-	<div>
-	<p id = "sidetext">The power of email all in a single account.</p>
-	<img id = "ioslogo" src="img/signuplogos/ios.png" alt = "IOS Mail Logo"> -->
-	<img id = "gmaillogo" src="img/signuplogos/gmail.png" alt = "Gmail Logo">
-	<img id = "yahoologo" src="img/signuplogos/yahoo.png" alt = "Yahoo Mail Logo">
-	<img id = "outlooklogo" src="img/signuplogos/outlook.jpg" alt = "Outlook Logo"> 
-	</div>
-
-	<form name = "signupform" onsubmit = "return(validation());" >
-		Name:<br>
-		<input class = "name" type = "text" name = "firstname" placeholder = "First" >
-		<input class = "name"type = "text" name = "lastname" placeholder = "Last" ><br><br>
-    
-    	Email Address:<br>
-		<input class = "samebox" type = "text" name = "email" placeholder = "Email" autocomplete="on"><br><br>
-	
-		Username:<br>
-		<input class = "samebox" type = "text" name = "username" placeholder = "Username" autocomplete="off"><br><br>
-
-		Password:<br>
-		<input class = "samebox" type = "password" name = "password" placeholder = "Password" autocomplete="off" ><br><br>
-
-		Security Question (case sensitive):<br>
-    	<select id = "dropbox" name = "security_questions">
-    	<option value = "number_1 "> What was your mother's maiden name?</option>
-    	<option value="saab">What was your childhood nickname?</option>
-    	<option value="fiat">What school did you attend in 6th grade?</option>
-    	<option value="audi">What was the model of your first car?</option>
-    	</select>
-    	<br><br>
-    	
-    	<input class = "samebox" type = "text" name = "security_answer" placeholder = "Answer" autocomplete="off" ><br><br>
-
-		<input id = "submit" type="submit">
-	</form>
-
 </body>
 </html>
 
