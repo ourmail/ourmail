c4969e5c5bcacf8e0fffde6968ce46e607c8d94d
tonyhongcode <tony.hong.code@outlook.com>
2015-10-27 14:23:00 -0700

    added change username

--- public/js/login.js
+++ public/js/login.js
@@ -2,49 +2,128 @@ $(function() {
 "use strict";
 	Parse.$ = jQuery;
 
-	Parse.initialize("9syVLvZdcJKZD9uAlCoYMKwjtmWxPHFhD4DdYKcN", "HH4p0QrjdzsO74KsoLhhhUZnPYDwExnZ8o9CCAeN");
+	Parse.initialize("21WjmmvR9bBPQi7sFjFVECL0tO1kETVsqtgHbJOA", "FeX6bK9Utc0fuBt9KpBqSzgFOskmHgHS04f2k6mC");
 	var Post = Parse.Object.extend("Post");
+
+	// ************ P A R S E   V I E W *********************
+	var LoginView = Parse.View.extend({
+			template: Handlebars.compile($('#login-tpl').html()),
+			events: {
+				'submit .form-signin': 'login'
+			},
+			login: function(e) {
+		 
+				// Prevent Default Submit Event
+				e.preventDefault();
+		 
+				// Get data from the form and put them into variables
+				var data = $(e.target).serializeArray(),
+					username = data[0].value,
+					password = data[1].value;
+		 
+				// Call Parse Login function with those variables
+				Parse.User.logIn(username, password, {
+					// If the username and password matches
+					success: function(user) {
+						alert('Welcome!');
+					},
+					// If there is an error
+					error: function(user, error) {
+						console.log(error);
+					}
+				});
+			},	
+			render: function(){
+				this.$el.html(this.template());
+			}
+		}),
+		WelcomeView = Parse.View.extend({
+			template: Handlebars.compile($('#welcome-tpl').html()),
+			render: function(){
+				var attributes = this.model.toJSON();
+				this.$el.html(this.template(attributes));
+			}
+		});	
 	
+	// ************ C U R R E N T   U S E R *******************
 	function displayCurrentUser() {
 		if (Parse.User.current()) {
 			console.log("Logged in by "+Parse.User.current().get("username"));
-			$("#current-user").html(Parse.User.current().get("username"));
+			$("#current-user").html("Current User is "+Parse.User.current().get("username"));
 		}
 		else {
-			$("#current-user").html("none");
+			$("#current-user").html("Current User is no one.");
 		}
 	}
 	
 	displayCurrentUser();
 	
-	//logout 
+	// ***************** L O G O U T ********************
 	// when this runs, jquery creates an event listener for the function
-	$("#logout").click(function(event) {				
+	$("#logout").click(function(event) {
 		Parse.User.logOut();
 		displayCurrentUser();
 		console.log("logout success.");
-	});
+	})
 	
-	//submit action for login
-	$("#login").submit(function(event){
+	// ****************** L O G I N *************************
+	$("#login").submit(function(event) {
 		event.preventDefault(); //prevent browser to refress and erase the js after the submit is pressed
 		
-		var username=$("#login-username").val();
-		var password=$("#login-password").val();
-		
-		Parse.User.logIn(username, password, {
+		var user = new Parse.User();
+		user.set("username", $("#loginUsername").val());
+		user.set("password", $("#loginPassword").val());
+		user.logIn({
 			success: function(user) { //on success, user object is pass back to me
-				console.log("login success.");
-				$.post('login.php', {'authentication' : true}, function(data) {
-					if(data.success == true) {
-						location.reload(true);
-					}					
-				}, 'json');
-				displayCurrentUser();
+				location.reload();
+				//window.location.href = '.html'; // go to main page
 			},
 			error: function(user, error) {
 				console.log("login error:"+error.message);
+				alert("Error: " + error.code + " " + error.message);
 			}
 		});
+	});
+	
+	// ******************** S I G U P ************************
+	$("#signup").submit(function(event){
+		event.preventDefault();
+		
+		var newUser = new Parse.User();
+		newUser.set("username", $("#signupUsername").val());
+		newUser.set("password", $("#signupPassword").val());
+		newUser.set("email", $("#signupEmail").val());
+		user.signUp({
+			success: function(user) {
+				displayCurrentUser();
+			},
+			error: function(user, error) {
+				console.log("signuperror:"+error.message);
+			}
+		});
+	});
+	
+	// *********** A C C O U N T   M A N A G E M E N T **********
+	$("#accountManagement").submit(function(event){
+		event.preventDefault();
+		
+		var currentUser= Parse.User.current();
+		if (currentUser){
+			//change username and password
+			currentUser.set("username", $("#newUsername").val());
+			currentUser.set("email", $("#newEmail").val());
+			currentUser.save({
+				success: function(user) {
+					displayCurrentUser();
+				},
+				error: function(user, error) {
+					alert("Error: " + error.code + " " + error.message);
+				}
+			});
+
+		} else {
+			//show the signup or login page
+			
+		}
 	});
 });
---
+++ public/newUsername.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Test</title>
+		
+		<!-- Parse Script -->
+		<script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+		<script src="./js/underscore.js"></script>
+		<script src = "http://www.parsecdn.com/js/parse-latest.js"></script>
+		<script src="./js/login.js"></script>	   
+	</head>
+
+	<body>
+	<div id="main">
+		<div id="current-user"></div>
+		<h1>Account Management</h1>
+		<form id="accountManagement">
+			<input type="text" id="newUsername" placeholder="New Username" class="form-control">
+			<input type="text" id="newEmail" placeholder="New Email" class="form-control">
+			<input type="submit" id="accountSubmit">
+		</form>
+		
+		
+	</div>
+
+	</body>
+</html>
