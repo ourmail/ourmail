b2dd3169b63001106c09cc7f0727a0da465c57b4
tonyhongcode <tony.hong.code@outlook.com>
2015-10-27 17:58:29 -0700

    Removed Experimental Code
    forgot to remove from last sync

--- public/js/login.js
+++ public/js/login.js
@@ -5,46 +5,6 @@ $(function() {
 	Parse.initialize("21WjmmvR9bBPQi7sFjFVECL0tO1kETVsqtgHbJOA", "FeX6bK9Utc0fuBt9KpBqSzgFOskmHgHS04f2k6mC");
 	var Post = Parse.Object.extend("Post");
 
-	// ************ P A R S E   V I E W *********************
-	var LoginView = Parse.View.extend({
-			template: Handlebars.compile($('#login-tpl').html()),
-			events: {
-				'submit .form-signin': 'login'
-			},
-			login: function(e) {
-		 
-				// Prevent Default Submit Event
-				e.preventDefault();
-		 
-				// Get data from the form and put them into variables
-				var data = $(e.target).serializeArray(),
-					username = data[0].value,
-					password = data[1].value;
-		 
-				// Call Parse Login function with those variables
-				Parse.User.logIn(username, password, {
-					// If the username and password matches
-					success: function(user) {
-						alert('Welcome!');
-					},
-					// If there is an error
-					error: function(user, error) {
-						console.log(error);
-					}
-				});
-			},	
-			render: function(){
-				this.$el.html(this.template());
-			}
-		}),
-		WelcomeView = Parse.View.extend({
-			template: Handlebars.compile($('#welcome-tpl').html()),
-			render: function(){
-				var attributes = this.model.toJSON();
-				this.$el.html(this.template(attributes));
-			}
-		});	
-	
 	// ************ C U R R E N T   U S E R *******************
 	function displayCurrentUser() {
 		if (Parse.User.current()) {
