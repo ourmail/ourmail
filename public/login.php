63a690b3d01c67c464d0c2d97fecdc9221d897e3
Jason Goulding <jgoul004@ucr.edu>
2015-11-05 09:29:35 -0800

    Session storage of user information on login

--- public/login.php
+++ public/login.php
@@ -3,6 +3,11 @@ session_start();
 if(isset($_POST['authentication'])) {
 	if($_POST['authentication'] == true) {
 		$_SESSION['userauth'] = true;
+		$_SESSION['username'] = $_POST['username'];
+		$_SESSION['email'] = $_POST['email'];
+		$_SESSION['firstname'] = $_POST['firstname'];
+		$_SESSION['lastname'] = $_POST['lastname'];
+		
 		$returnArray = array('success' => true);
 		echo json_encode($returnArray);
 	}	
